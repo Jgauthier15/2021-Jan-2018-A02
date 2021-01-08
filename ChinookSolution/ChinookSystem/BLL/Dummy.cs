56ffee1e3fae42a6721c235e7110a9ae99192a54
Joel Gauthier <jgauthier15@nait.ca>
2021-01-08 16:03:28 -0700

    NuGet EntityFrameWork
    Installed EntityFramwork
    Added Reference to ChinookSystem
    System_EntityFramwork

--- ChinookSolution/ChinookSystem/BLL/Dummy.cs
+++ ChinookSolution/ChinookSystem/BLL/Dummy.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.BLL
 {
-    class Class1
+    class Dummy
     {
     }
 }
--- ChinookSolution/ChinookSystem/DAL/Dummy.cs
+++ ChinookSolution/ChinookSystem/DAL/Dummy.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.DAL
 {
-    class Class1
+    class Dummy
     {
     }
 }
