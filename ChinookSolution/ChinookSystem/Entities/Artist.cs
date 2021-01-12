087aea284ad8caa026a17c4a986bcc51ed5a6e18
Joel Gauthier <jgauthier15@nait.ca>
2021-01-12 13:48:59 -0700

    Create Artist Entity
    Using namespaces
    [Key]
    Annotation Validation
    Fully Implemented Nullable Property

---
+++ ChinookSolution/ChinookSystem/Entities/Artist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Artists")]
+    internal class Artist
+    {
+        private string _Name;
+
+       [Key]
+        public int AristId { get; set; }
+
+
+        //Annotational Validation for Name
+        [StringLength(120, ErrorMessage= "Artist name is limited to 120 characters.")]
+
+        //Name can be nullable, so let's fully implement it. (private string _Name; )
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = string.IsNullOrEmpty(value) ? null : value; }
+        }
+    }
+}
